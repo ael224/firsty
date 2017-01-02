1e56422043ede24a40d46a7e12ea6ce8003538eb
BuildTools <unconfigured@null.spigotmc.org>
2017-01-02 19:02:40 +1100

    added some more

--- get-image.js
+++ get-image.js
@@ -2,10 +2,11 @@ var url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat"
 
 fetch(url)
     .then(function (response) {
-        return response.text()
+        return response.json()
     })
-    .then(function (text) {
-        console.log(text)
+    .then(function (json) {
+        console.log(json)
+        console.log(json.data.url)
     })
     .catch(error => {
         console.log(error)
